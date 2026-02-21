e350ff152bbc1772c3ecc5e8cf661eb955bf8819
Stiven Giraldo <giraldostiven150@gmail.com>
2026-02-21 11:07:02 -0500

    ejercicio del libro

---
+++ src/main/java/com/example/Libro.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private int paginas;
+
+    public Libro() {
+        this.titulo = "Desconocido";
+        this.autor = "Anónimo";
+        this.paginas = 0;
+    }
+    public Libro (String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = 0;
+    }
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+    public void mostrarDetalles(){
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Número de páginas: " + paginas);
+    }
+}
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,14 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+        Libro libro1 = new Libro();
+        libro1.mostrarDetalles();
+        System.out.println();
+        Libro libro2 = new Libro("Satanas", "Mario Mendoza");
+        libro2.mostrarDetalles();
+        System.out.println();
+        Libro libro3 = new Libro("De mi, Para mi", "Nacarid Portal", 473);
+        libro3.mostrarDetalles();
        
     }
 }
